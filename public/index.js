361c4d45c4523f259ce42d7cbd3822fbb40d0e7c
Eleftheria Koronidi <ria@koronidi.com>
2018-04-11 20:54:37 +0100

    Tidy up the code and comments in the index.js file

--- public/index.js
+++ public/index.js
@@ -14,16 +14,21 @@ const requestComplete = function(){
 
 const populateList = function(beers){
   const ul = document.getElementById('beer-list');
-  debugger;
-
   for(let beer of beers){
     displayBeerDetails(beer);
-    // save(beer);
   }
 };
 
 const displayBeerDetails = function(beer){
   const ul = document.getElementById('beer-list');
+
+  // var ingredientsArray = [];
+  // ingredientsArray.push(beer.ingredients);
+  // for(element of ingredientsArray){
+  //   let li = document.createElement('li');
+  //   li.innerText = element;
+  //   ul.appendChild(li);
+  // }
   const li = document.createElement('li');
   const img = document.createElement('img');
   li.innerText = beer.name;
@@ -32,51 +37,10 @@ const displayBeerDetails = function(beer){
   ul.appendChild(li);
 };
 
-// var save = function (beer) {
-//   var beersArray = JSON.parse(localStorage.getItem('toDoList')) || [];
-//   // - add the newItem to the array
-//   todosArray.push(newItem);
-//   // - stringify the updated array
-//   // var jsonString = JSON.stringify(todosArray);
-//   // - save it back to localstoage
-//   // localStorage.setItem('todoList', jsonString);
-//
-//   // or do it in one line
-//   localStorage.setItem('toDoList', JSON.stringify(todosArray));
-// }
-
-// const handleSelectChanged = function(event){
-//   var pTag = document.querySelector('#select-result');
-//   pTag.innerText = this.value;
-//
-//   const select = document.querySelector('select');
-//   var savedCountry = select.value;
-//   var jsonString = JSON.stringify(savedCountry);
-//   localStorage.setItem('savedCountry', jsonString);
-// };
-//
-// const saveCountry = function(countries, savedCountry){
-//   for(let country of countries){
-//     if(savedCountry.name === country.name){
-//       var jsonString = JSON.stringify(country);
-//       localStorage.setItem('country', jsonString);
-//     }
-//
-//   }
-//   const select = document.querySelector('select');
-//   var savedCountry = select.value;
-//   var jsonString = JSON.stringify(savedCountry);
-//   localStorage.setItem('savedCountry', jsonString);
-// }
-
 
 const app = function(){
   const url = "https://api.punkapi.com/v2/beers";
   makeRequest(url, requestComplete);
-
-  // const select = document.querySelector('select');
-  // select.addEventListener('change', handleSelectChanged);
-
 }
 
 window.addEventListener('load', app);
